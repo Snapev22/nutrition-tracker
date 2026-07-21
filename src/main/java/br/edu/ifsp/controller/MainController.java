842a89dbc2a903032abd574bdf07099ac004e6bf
Snapev22 <vitorhshelby@outlook.com>
2026-07-21 19:22:00 -0300

    substitui printStackTrace por SLF4J/Logback

--- src/main/java/br/edu/ifsp/controller/MainController.java
+++ src/main/java/br/edu/ifsp/controller/MainController.java
@@ -6,10 +6,14 @@ import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
+import org.slf4j.Logger;
+
 
 public class MainController {
+
+    private static final Logger logger = LoggerFactory.getLogger(MainController.class);
 
     @FXML
     private StackPane paneCentral;
@@ -57,7 +61,7 @@ public class MainController {
             Node tela = loader.load();
             paneCentral.getChildren().setAll(tela);
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error("Erro ao carregar tela", e);
 
             Alert alert = new Alert(
                     Alert.AlertType.ERROR,
