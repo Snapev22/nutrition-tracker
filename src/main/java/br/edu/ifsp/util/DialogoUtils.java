0fb543e9010b7a3801de008afd0c69360f093a9f
Snapev22 <vitorhshelby@outlook.com>
2026-07-21 19:39:15 -0300

    adciona método util para confirmação de exclusão

--- src/main/java/br/edu/ifsp/util/DialogoUtils.java
+++ src/main/java/br/edu/ifsp/util/DialogoUtils.java
@@ -1,4 +1,19 @@
 package br.edu.ifsp.util;
 
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+
+import java.util.Optional;
+
 public class DialogoUtils {
+    public static boolean confirmarExclusao(String nomeEntidade){
+        Alert confirmacao = new Alert(Alert.AlertType.CONFIRMATION,
+                "Tem certeza que deseja excluir \"" + nomeEntidade + "\"? Essa ação não pode ser desfeita."
+        );
+        confirmacao.setTitle("Confirmar exclusão");
+        confirmacao.setHeaderText(null);
+
+        Optional<ButtonType> resultado = confirmacao.showAndWait();
+        return resultado.isPresent() && resultado.get() == ButtonType.OK;
+    }
 }
