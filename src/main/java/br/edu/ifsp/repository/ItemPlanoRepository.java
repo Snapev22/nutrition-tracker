4e82aa97237ddd0ed6ff8cd539eab11e47961570
Snapev22 <vitorhshelby@outlook.com>
2026-08-15 00:57:10 -0300

    feat(jpa): implementa cálculo de resumo nutricional via constructor expression JPQL com SUM agregado

--- src/main/java/br/edu/ifsp/repository/ItemPlanoRepository.java
+++ src/main/java/br/edu/ifsp/repository/ItemPlanoRepository.java
@@ -1,7 +1,10 @@
 package br.edu.ifsp.repository;
 
+import br.edu.ifsp.model.InformacaoNutricional;
 import br.edu.ifsp.model.ItemPlano;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -9,4 +12,16 @@ public interface ItemPlanoRepository extends JpaRepository<ItemPlano, Long> {
 
 
     List<ItemPlano> findByPlanoDiarioId(Long planoDiarioId);
+
+    @Query("""
+            SELECT new br.edu.ifsp.model.InformacaoNutricional(
+                COALESCE(SUM(i.totalNutricional.calorias), 0),
+                COALESCE(SUM(i.totalNutricional.proteina), 0),
+                COALESCE(SUM(i.totalNutricional.carboidrato), 0),
+                COALESCE(SUM(i.totalNutricional.gordura), 0)
+            )
+            FROM ItemPlano i
+            WHERE i.planoDiario.id = :planoId
+            """)
+    InformacaoNutricional somarResumoPorPlano(@Param ("planoId") Long planoId);
 }
